27eabefc2aa32c879e0c95cda2b047641bb32491
Cichopek, Bartlomiej <bartlomiej.cichopek@capgemini.com>
2018-07-11 08:12:02 +0200

    Bot that should be able to recognize and consume BuiltInDatetimeV2Types.DateTimeRange

---
+++ BotWithDateTimeV2DateTimeRange/Actions/FindHotelAction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot.Builder.CognitiveServices.LuisActionBinding;
+using System;
+using System.Threading.Tasks;
+
+namespace BotWithDateTimeV2DateTimeRange.Actions
+{
+    [Serializable]
+    [LuisActionBinding("FindHotel", FriendlyName = "FindHotel")]
+    public class FindHotelAction : BaseLuisAction
+    {
+        [LuisActionBindingParam(BuiltinType = BuiltInDatetimeV2Types.DateTimeRange, Order = 1)]
+        public DateTime? When { get; set; }
+
+        public override Task<object> FulfillAsync()
+        {
+            return Task.FromResult<object>(this);
+        }
+    }
+}
---
+++ BotWithDateTimeV2DateTimeRange/Dialogs/RootDialog.cs
@@ -0,0 +1,32 @@
+﻿using BotWithDateTimeV2DateTimeRange.Actions;
+using Microsoft.Bot.Builder.CognitiveServices.LuisActionBinding.Bot;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BotWithDateTimeV2DateTimeRange.Dialogs
+{
+    [Serializable]
+    public class RootDialog : LuisActionDialog<object>
+    {
+        public RootDialog(IEnumerable<Assembly> assemblies, params ILuisService[] services) : base(assemblies, services)
+        {
+        }
+
+        protected override async Task NoActionDetectedAsync(IDialogContext context, IMessageActivity message)
+        {
+            await context.PostAsync("I dont understand");
+        }
+
+        [LuisIntent("FindHotel")]
+        public async Task FindHotelIntent(IDialogContext context, object result)
+        {
+            var res = result as FindHotelAction;
+            await context.PostAsync($"{res.When}");
+        }
+    }
+}
